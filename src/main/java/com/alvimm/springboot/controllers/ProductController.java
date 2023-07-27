3716b469c7da943c2ccfb2d940d6ddb5177485e8
Filipe Alvim <filipe2012alvim@gmail.com>
2023-07-27 16:27:42 -0300

    Controller creation

---
+++ src/main/java/com/alvimm/springboot/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package com.alvimm.springboot.controllers;
+
+import com.alvimm.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+
+}
