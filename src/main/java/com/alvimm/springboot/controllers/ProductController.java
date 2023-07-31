87f91e6e4d76e8c0c1073a0fb7d730ac8fbdc3c3
Filipe Alvim <filipe2012alvim@gmail.com>
2023-07-31 12:37:32 -0300

    DELETE method

--- src/main/java/com/alvimm/springboot/controllers/ProductController.java
+++ src/main/java/com/alvimm/springboot/controllers/ProductController.java
@@ -33,22 +33,32 @@ public class ProductController {
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
-        Optional<ProductModel> product0 = productRepository.findById(id);
-        if(product0.isEmpty()){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
     @PutMapping("/products/{id}")
     public ResponseEntity<Object> updateProject(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductRecordDTO productRecordDTO){
-        Optional<ProductModel> product0 = productRepository.findById(id);
-        if(product0.isEmpty()){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
-        var productModel = product0.get();
+        var productModel = productO.get();
         BeanUtils.copyProperties(productRecordDTO, productModel);
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProject(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
     }
 
 }
